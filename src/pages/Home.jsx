a250f73f6291df99a4b5796f6b242e3d8212c1ea
Jessica Ramirez <ramzgonz.j@gmail.com>
2023-06-26 14:35:00 -0400

    Comprobando cambios en Netlify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className='home-container'>
         <h1>You got the travel plans, we got the travel vans.</h1>
-        <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-        <Link to='vans'>Find your van</Link>        
+        <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>        
+        <Link to='vans'>FIND YOUR VAN</Link>
     </div>
   )
 }
