a98c4245bc6f73dd71e947e933b85f4adfc8a711
Jessica Ramirez <ramzgonz.j@gmail.com>
2023-06-28 22:09:41 -0400

    Add HostVans page

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,34 @@ import { Routes, Route, Link, createBrowserRouter, createRoutesFromElements, Rou
 import Home from './pages/Home'
 import About from './pages/About'
 import Layout from './components/Layout'
-import Vans from './pages/Vans'
-import VanDetail from './pages/VanDetail'
+import Vans from './pages/Vans/Vans'
+import VanDetail from './pages/Vans/VanDetail'
+import Dashboard from './pages/Host/Dashboard'
+import Income from './pages/Host/Income'
+import Reviews from './pages/Host/Reviews'
+import HostLayout from './components/HostLayout'
+import HostVans from './pages/Host/HostVans'
+import HostVanDetail from './pages/Host/HostVanDetail'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Layout />}>
     <Route index element={<Home />} />
     <Route path='about' element={<About />} />
-    <Route path='vans' element={<Vans />} />
-    <Route path='vans/:id' element={<VanDetail />} />
-  
+    
+
+    <Route path='vans' >
+      <Route index element={<Vans />} />
+      <Route path=':id' element={<VanDetail />} />
+    </Route>
+
+    <Route path='host' element={<HostLayout />}>
+      <Route index element={<Dashboard />} />
+      <Route path='income' element={<Income />} />
+      <Route path='reviews' element={<Reviews />} />
+      <Route path='vans' element={<HostVans />} />
+      <Route path='vans/:id' element={<HostVanDetail />} />
+
+    </Route>   
   </Route>
 ))
 
